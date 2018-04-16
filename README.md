2ac11199c5cab71d0cfb65cb809488f65e25ab3e
Cai <zw.cai@mediatek.com>
2018-04-16 16:43:46 +0800

    Update formula.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # homebrew-nxcapsrv
-A Nintendo Switch Album Tool.
+
+A Nintendo Switch album tool.
+
+`brew install https://github.com/x43x61x69/homebrew-nxcapsrv/master/formula.rb`
---
+++ formula.rb
@@ -0,0 +1,10 @@
+class nxcapsrv < Formula
+  desc "A Nintendo Switch album tool."
+  homepage "https://github.com/x43x61x69/homebrew-nxcapsrv"
+  url ""
+  sha256 ""
+
+  def install
+    bin.install Dir["bin/*"]
+  end
+end
