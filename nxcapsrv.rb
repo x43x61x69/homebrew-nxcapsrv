8e1889cfd3ab0ea8f83d898eafe0756441945849
Zhi-Wei Cai <support@vox.vg>
2018-04-16 22:00:10 +0800

    Update formula.

--- nxcapsrv.rb
+++ nxcapsrv.rb
@@ -1,11 +1,10 @@
 class Nxcapsrv < Formula
   desc "A Nintendo Switch album tool."
   homepage "https://github.com/x43x61x69/homebrew-nxcapsrv"
-  url "https://github.com/x43x61x69/homebrew-nxcapsrv/raw/master/nxcapsrv-v0.0.1.tar.gz"
-  sha256 "c53117e93316265208b04b88d15f4ca2187ab27c29093d0f18bcc2ade3d54463"
+  url "https://github.com/x43x61x69/homebrew-nxcapsrv/raw/master/nxcapsrv-v0.0.2.tar.gz"
+  sha256 "e8dd85b00da1e5ce43a17cb226545b8cad061b0d5c911fe7d8592e875a150e8e"
 
   def install
-    # bin.install Dir["*"]
-    bin.install "capsrv"
+     bin.install Dir["*"]
   end
 end
